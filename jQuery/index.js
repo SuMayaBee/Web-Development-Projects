8af4360c2f061ebd2ea90a2be7157deb3411e0ab
Sumaiya Islam <sumaiyaislam-14-2021711385@it.du.ac.bd>
2024-04-30 12:42:21 +0600

    Add files via upload

---
+++ jQuery/index.js
@@ -0,0 +1,10 @@
+
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
+
+// $(document).keydown(function(event){
+//     //console.log(event.key);
+//     $("h1").html(event.key);
+// })
+
